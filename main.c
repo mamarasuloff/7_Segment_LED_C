ff5b58f1718d2e2433b96a565186894f49336b51
zukhriddin <77203422+mamarasuloff@users.noreply.github.com>
2022-04-12 15:06:10 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * 7_Segment_LED_C.cpp
  *
- * Created: 06.09.2017 11:08:35
+ * Created: 12.04.2022 11:08:35
  *  Author: Mamarasuloff
  */ 
 
@@ -13,22 +13,22 @@
 #include "74HC595.h"
 
 int Data[] = { 0xBD, 0x90, 0x3B, 0xBA, 0x96,	    // { '0', '1', '2', '3', '4', 
-               0xAE, 0xAF, 0x98, 0xBF, 0xBE };		//   '5', '6', '7', '8', '9' };
+               0xAE, 0xAF, 0x98, 0xBF, 0xBE };	    //   '5', '6', '7', '8', '9' };
 unsigned char Segments[2];
 			  
 int main(void) {
-	DDRB = 0xFF;
-	void F74HC595_Init(void);
+    DDRB = 0xFF;
+    void F74HC595_Init(void);
     while(1) {
         //TODO:: Please write your application code
-		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 10; j++) {
-				Segments[0] = Data[i];
-				Segments[1] = Data[j];
-			    Indication(Segments, 2);
-			    _delay_ms(100);
-			}
-		}
+        for(int i = 0; i < 10; i++) {
+	    for(int j = 0; j < 10; j++) {
+		Segments[0] = Data[i];
+		Segments[1] = Data[j];
+		Indication(Segments, 2);
+         	_delay_ms(100);
+            }
+	}
     }
 	return 0;
 }
